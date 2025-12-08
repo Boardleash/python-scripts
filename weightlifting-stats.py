63a0565591250c3cc504dead37476dcc28d10eae
VM Administrator <valravn@summit.deep.loc>
2025-12-08 13:59:27 -0500

    Cleaned up code, removed redundancies, made more easily readable

--- weightlifting-stats.py
+++ weightlifting-stats.py
@@ -12,106 +12,57 @@ import mariadb
 import pandas as pd
 import readline
 import sqlalchemy as db
-import sys
-
-#----------------------------------------------------------
-# Standard DB Connector Configuration using MariaDB library
-#----------------------------------------------------------
-
-#db_config = { 
-#        'host': 'localhost',
-#        'port': '3306',
-#        'user': 'testuser',
-#        'password': 'test',
-#        'database': 'fitness'}
 
 #------------------------------
-# DB Connector using SQLAlchemy
+# DB Connector Using SQLAlchemy 
 #------------------------------
-
 engine = db.create_engine('mariadb+mariadbconnector://testuser:test@localhost:3306/fitness')
 connection = engine.connect()
 
-user_request = input("What data would you like to see (AVERAGE or TOTAL)?: ")
-
-#------------------------
-# USING MARIADB CONNECTOR
-#------------------------
-
-# Query, error handling and connection closing using "normal" MariaDB connection
-#try:
-#  connection =  mariadb.connect(**db_config)
-#  cursor = connection.cursor()
-#  cursor.execute("select * from weightlifting where muscle_group='legs'")
-#  for row in cursor:
-#    print(row)
-#except mariadb.Error as e:
-#  print(f"Error connecting to MariaDB: {e}")
-#  sys.exit(1)
-#finally:
-#  if 'cursor' in locals() and cursor:
-#    cursor.close()
-#  if 'conn' in locals() and conn:
-#    conn.close()
-#    print("Connection closed.")
-
-#-----------------
-# USING SQLALCHEMY
-#-----------------
-
-if user_request.lower()  == "average":
-  query_six = "select avg(weight_lbs) from weightlifting"
-  df_six = pd.read_sql_query(query_six,connection).to_string(index=False,header=None)
-  print(f"The average amount of weight lifted for ALL LIFTS is {df_six} pounds.")
-
-  query = "select avg(weight_lbs) from weightlifting where muscle_group='legs'"
-  df = pd.read_sql_query(query,connection).to_string(index=False,header=None)
-  print(f"The average amount of weight lifted for LEGS is {df} pounds.")
-
-  query_two = "select avg(weight_lbs) from weightlifting where muscle_group='back'"
-  df_two = pd.read_sql_query(query_two,connection).to_string(index=False,header=None)
-  print(f"The average amount of weight lifted for BACK is {df_two} pounds.")
-
-  query_three = "select avg(weight_lbs) from weightlifting where muscle_group='chest'"
-  df_three = pd.read_sql_query(query_three,connection).to_string(index=False,header=None)
-  print(f"The average amount of weight lifted for CHEST is {df_three} pounds.")
-
-  query_four = "select avg(weight_lbs) from weightlifting where muscle_group='shoulders'"
-  df_four = pd.read_sql_query(query_four,connection).to_string(index=False,header=None)
-  print(f"The average amount of weight lifted for SHOULDERS is {df_four} pounds.")
-
-  query_five = "select avg(weight_lbs) from weightlifting where muscle_group='arms'"
-  df_five = pd.read_sql_query(query_five,connection).to_string(index=False,header=None)
-  print(f"The average amount of weight lifted for ARMS is {df_five} pounds.")
-elif user_request.lower() == "total":
-  query = "select sum(weight_lbs) from weightlifting"
-  df = pd.read_sql_query(query,connection).to_string(index=False,header=None)
-  print(f"The sum of ALL weight lifted is {df} pounds.")
-
-  query_two = "select sum(weight_lbs) from weightlifting where muscle_group='legs'"
-  df_two = pd.read_sql_query(query_two,connection).to_string(index=False,header=None)
-  print(f"The sum of weight lifted for LEGS is {df_two} pounds.")
-
-  query_three = "select sum(weight_lbs) from weightlifting where muscle_group='back'"
-  df_three= pd.read_sql_query(query_three,connection).to_string(index=False,header=None)
-  print(f"The sum of weight lifted for BACK is {df_three} pounds.")
-
-  query_four = "select sum(weight_lbs) from weightlifting where muscle_group='chest'"
-  df_four = pd.read_sql_query(query_four,connection).to_string(index=False,header=None)
-  print(f"The sum of weight lifted for CHEST is {df_four} pounds.")
-
-  query_five = "select sum(weight_lbs) from weightlifting where muscle_group='shoulders'"
-  df_five = pd.read_sql_query(query_five,connection).to_string(index=False,header=None)
-  print(f"The sum of weight lifted for SHOULDERS is {df_five} pounds.")
-
-  query_six= "select sum(weight_lbs) from weightlifting where muscle_group='arms'"
-  df_six = pd.read_sql_query(query_six,connection).to_string(index=False,header=None)
-  print(f"The sum of weight lifted for ARMS is {df_six} pounds.")
-else:
-  print("That is not a valid entry.")
-
-# Close connection to database
+#------ GENERAL DATABASE QUERY ---------
+query = "select * from weightlifting;"
+df = pd.read_sql_query(query,connection)
 connection.close()
-print("Database connection has been closed.")
+#---------------------------------------
+
+def avgStats():
+  avg_all = round(df['weight_lbs'].mean(),1)
+  avg_legs = round(df[df['muscle_group'].str.contains('Legs')]['weight_lbs'].mean(),1)
+  avg_back = round(df[df['muscle_group'].str.contains('Back')]['weight_lbs'].mean(),1)
+  avg_chest = round(df[df['muscle_group'].str.contains('Chest')]['weight_lbs'].mean(),1)
+  avg_shoulders = round(df[df['muscle_group'].str.contains('Shoulders')]['weight_lbs'].mean(),1)
+  avg_arms = round(df[df['muscle_group'].str.contains('Arms')]['weight_lbs'].mean(),1)
+
+  print(f"The average amount of weight lifted for ALL LIFTS is {avg_all} pounds.\n"
+        f"The average amount of weight lifted for LEGS is {avg_legs} pounds.\n"
+        f"The average amount of weight lifted for BACK is {avg_back} pounds.\n"
+        f"The average amount of weight lifted for CHEST is {avg_chest} pounds.\n"
+        f"The average amount of weight lifted for SHOULDERS is {avg_shoulders} pounds.\n"
+        f"The average amount of weight lifted for ARMS is {avg_arms} pounds.")
+
+def sumStats():
+  sum_all = round(df['weight_lbs'].sum(),1)
+  sum_legs = round(df[df['muscle_group'].str.contains('Legs')]['weight_lbs'].sum(),1)
+  sum_back = round(df[df['muscle_group'].str.contains('Back')]['weight_lbs'].sum(),1)
+  sum_chest = round(df[df['muscle_group'].str.contains('Chest')]['weight_lbs'].sum(),1)
+  sum_shoulders = round(df[df['muscle_group'].str.contains('Shoulders')]['weight_lbs'].sum(),1)
+  sum_arms = round(df[df['muscle_group'].str.contains('Arms')]['weight_lbs'].sum(),1)
+
+  print(f"The sum of ALL weight lifted is {sum_all} pounds.\n"
+        f"The sum of weight lifted for LEGS is {sum_legs} pounds.\n"
+        f"The sum of weight lifted for BACK is {sum_back} pounds.\n"
+        f"The sum of weight lifted for CHEST is {sum_chest} pounds.\n"
+        f"The sum of weight lifted for SHOULDERS is {sum_shoulders} pounds.\n"
+        f"The sum of weight lifted for ARMS is {sum_arms} pounds.")
+
+#---- SCRIPT EXECUTION ----
+question = input("What data would you like to see (AVERAGE or TOTAL)?: ")
+if question.lower()  == "average":
+  avgStats()
+elif question.lower() == "total":
+  sumStats()
+else:
+  print("That is not a valid response.")
+  exit
 
 # EOF
