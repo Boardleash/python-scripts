143d662318d93b27d75dcf19318f31cb2ef10672
valravn <valravn@demersal.deep.loc>
2025-12-09 11:58:11 -0500

    Cleaned up income-calc script.  Removed under-construction directory

---
+++ income-calc.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+#----------------------------
+# TITLE: income-calc.py
+# AUTHOR: Boardleash (Derek)
+# DATE: Monday, July 7 2025
+#----------------------------
+
+# Simple Python script to provide some income based answers based on user response
+
+import readline
+
+def hourlyKnown():
+  '''If the hourly amount earned is known, this function will work with that.'''
+  biweekly = round((hourly * 80),2)
+  monthly = round((biweekly * 2),2)
+  annual = round((monthly * 12),2)
+  monthly_rent = round(((annual * 0.30) / 12),2)
+
+  print(f"Based solely on your HOURLY income, here is the layout of your income, excluding taxes:\n"
+        f"${biweekly} is what you earn every two weeks.\n"
+        f"${monthly} is what you bring home every month.\n"
+        f"${annual} is what you make a year.\n"
+        f"Using the 30% rule, you can afford ${monthly_rent} in rent a month.")
+
+def annualKnown():
+  '''If the annual income is known, this function will work with that.'''
+  monthly = round((annual / 12),2)
+  biweekly = round((monthly / 2),2)
+  hourly = round((biweekly / 80),2)
+  monthly_rent = round(((annual * 0.30) / 12),2)
+
+  print(f"Based solely on your ANNUAL income, here is the layout of your income, excluding taxes:\n"
+        f"${hourly} is what you make an hour.\n"
+        f"${biweekly} is what you earn every two weeks.\n"
+        f"${monthly} is what you bring home every month.\n"
+        f"Using the 30% rule, you can afford ${monthly_rent} in rent a month.")
+
+#------- SCRIPT EXECUTION ----------
+print(f"Hello, I can perform some basic income calcualtions for you.\n"
+      f"In order to do this, I will ask you two questions.  Please exclude commas from your input.\n"
+      f"For example, rather than type 70,000.00, please type 70000.00.")
+
+question = input("Which income amount are you able to provide?:\na. Annual\nb. Hourly\nYour response: ")
+
+if question.lower() == 'a' or question.lower() == 'annual':
+  annual = input("Please enter your annual income amount: ")
+  if annual.isalpha():
+    print("You have provided an invalid input.")
+  else:
+    annual = float(annual.replace(",","").replace("$",""))
+    annualKnown()
+elif question.lower() == 'b' or question.lower() == 'hourly':
+  hourly = input("Please enter what you make hourly: ")
+  if hourly.isalpha():
+    print("You have provided an invalid input.")
+  else:
+    hourly = float(hourly.replace(",","").replace("$",""))
+    hourlyKnown()
+else:
+  print("You did not provide a valid response.")
+  exit
+
+# EOF
