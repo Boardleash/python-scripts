8cc0f1485bf011cbc803bdc6ba13c0213e5f2ba8
valravn <valravn@demersal.deep.loc>
2025-12-12 12:45:31 -0500

    Changed monthly rent variable naming

--- income-calc.py
+++ income-calc.py
@@ -15,26 +15,26 @@ def hourlyKnown():
   biweekly = round((hourly * 80),2)
   monthly = round((biweekly * 2),2)
   annual = round((monthly * 12),2)
-  monthly_rent = round(((annual * 0.30) / 12),2)
+  monthlyRent = round(((annual * 0.30) / 12),2)
 
   print(f"Based solely on your HOURLY income, here is the layout of your income, excluding taxes:\n"
         f"${biweekly} is what you earn every two weeks.\n"
         f"${monthly} is what you bring home every month.\n"
         f"${annual} is what you make a year.\n"
-        f"Using the 30% rule, you can afford ${monthly_rent} in rent a month.")
+        f"Using the 30% rule, you can afford ${monthlyRent} in rent a month.")
 
 def annualKnown():
   '''If the annual income is known, this function will work with that.'''
   monthly = round((annual / 12),2)
   biweekly = round((monthly / 2),2)
   hourly = round((biweekly / 80),2)
-  monthly_rent = round(((annual * 0.30) / 12),2)
+  monthlyRent = round(((annual * 0.30) / 12),2)
 
   print(f"Based solely on your ANNUAL income, here is the layout of your income, excluding taxes:\n"
         f"${hourly} is what you make an hour.\n"
         f"${biweekly} is what you earn every two weeks.\n"
         f"${monthly} is what you bring home every month.\n"
-        f"Using the 30% rule, you can afford ${monthly_rent} in rent a month.")
+        f"Using the 30% rule, you can afford ${monthlyRent} in rent a month.")
 
 #------- SCRIPT EXECUTION ----------
 print(f"Hello, I can perform some basic income calculations for you.\nYou will be prompted for responses.  Please respond appropriately.\n")
