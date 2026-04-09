9f5ef2a4e4fd14060db73e594f263e0dc695b813
Boardleash <boardleash@gmail.com>
2026-04-09 09:49:58 -0400

    Cleaned up code

--- income-calc.py
+++ income-calc.py
@@ -1,62 +1,57 @@
 #!/usr/bin/env python3
-
 #----------------------------
 # TITLE: income-calc.py
 # AUTHOR: Boardleash (Derek)
 # DATE: Monday, July 7 2025
 #----------------------------
-
-# Simple Python script to provide some income based answers based on user response
-
-import readline
-
+#---------------------------- DESCRIPTION ------------------------------------
+# Python script to provide income based answers based on your response
+#-----------------------------------------------------------------------------
+#import readline
+#--- Hourly Function
 def hourlyKnown():
-  '''If the hourly amount earned is known, this function will work with that.'''
+  '''Work with known hourly earnings'''
   biweekly = round((hourly * 80),2)
   monthly = round((biweekly * 2),2)
   annual = round((monthly * 12),2)
   monthlyRent = round(((annual * 0.30) / 12),2)
-
-  print(f"Based solely on your HOURLY income, here is the layout of your income, excluding taxes:\n"
+  print(f"Based on your HOURLY income (excluding taxes):\n"
         f"${biweekly} is what you earn every two weeks.\n"
         f"${monthly} is what you bring home every month.\n"
         f"${annual} is what you make a year.\n"
         f"Using the 30% rule, you can afford ${monthlyRent} in rent a month.")
-
+#--- Annual Function
 def annualKnown():
-  '''If the annual income is known, this function will work with that.'''
+  '''Work with known annual earnings'''
   monthly = round((annual / 12),2)
   biweekly = round((monthly / 2),2)
   hourly = round((biweekly / 80),2)
   monthlyRent = round(((annual * 0.30) / 12),2)
-
-  print(f"Based solely on your ANNUAL income, here is the layout of your income, excluding taxes:\n"
+  print(f"Based on your ANNUAL income (excluding taxes):\n"
         f"${hourly} is what you make an hour.\n"
         f"${biweekly} is what you earn every two weeks.\n"
         f"${monthly} is what you bring home every month.\n"
         f"Using the 30% rule, you can afford ${monthlyRent} in rent a month.")
 
-#------- SCRIPT EXECUTION ----------
-print(f"Hello, I can perform some basic income calculations for you.\nYou will be prompted for responses.  Please respond appropriately.\n")
-
-question = input("Which income amount are you able to provide?:\na. Annual\nb. Hourly\nYour response: ")
-
+#--- Main Script
+question = input("Which income amount are you able to provide?: \
+  \na. Annual\nb. Hourly\nYour response: ")
 if question.lower() == 'a' or question.lower() == 'annual':
   annual = input("Please enter your annual income amount: ")
   if annual.isalpha():
-    print("You have provided an invalid input.")
+    print("Invalid input.")
   else:
     annual = float(annual.replace(",","").replace("$",""))
     annualKnown()
 elif question.lower() == 'b' or question.lower() == 'hourly':
   hourly = input("Please enter what you make hourly: ")
   if hourly.isalpha():
-    print("You have provided an invalid input.")
+    print("Invalid input.")
   else:
     hourly = float(hourly.replace(",","").replace("$",""))
     hourlyKnown()
 else:
-  print("You did not provide a valid response.")
+  print("Invalid response.")
   exit
 
 # EOF
